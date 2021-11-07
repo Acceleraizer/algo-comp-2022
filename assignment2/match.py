ae5c36318999608975ef391a3bd5b09c9ff8b338
Acceleraizer <wpwittyphantom@gmail.com>
2021-11-07 00:48:27 -0400

    v1

--- assignment2/match.py
+++ assignment2/match.py
@@ -21,7 +21,74 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             - What data structure can you use to take advantage of this fact when forming your matches?
         - This is by no means an exhaustive list, feel free to reach out to us for more help!
     """
-    matches = [()]
+    def pref_gender_mult(pref, gender):
+        if pref == "Men":
+            return 1 if gender == "Female" else 0.1
+        elif pref == "Women":
+            return 1 if gender == "Male" else 0.1
+        else:
+            return 1 if gender == "Non-binary" else 0.1
+
+    n = len(scores)
+    # random assign half the population to be proposers
+    props = list(np.random.choice(n, n//2, replace=False))
+    recvs = [i for i in range(n) if i not in props]
+    print(props)
+    print(recvs)
+
+    # get the scores assigned by proposers, assigning the correct multiplier based on gender preferences
+    # sorts the list in ascending order
+    # afterwards, drop the score information, leaving only a ranking
+    prop_scores = [ [(scores[props[i]][j]*pref_gender_mult(gender_pref[props[i]], gender_id[j]),j) for j in recvs] 
+        for i in range(n//2)]
+    for q in prop_scores:
+        q.sort()
+    prop_ls = list(map(lambda ls:list(map(lambda x:x[1], ls)), prop_scores))
+    print(prop_ls)
+
+    # same thing for receivers, but with a descending order sort
+    recvs_scores =  [ [(scores[recvs[i]][j]*pref_gender_mult(gender_pref[recvs[i]], gender_id[j]),j) for j in props] 
+        for i in range(n//2)]
+    for pr in recvs_scores:
+        pr.sort(reverse=True)
+    recvs_ls = list(map(lambda ls:list(map(lambda x:x[1], ls)), recvs_scores))
+    print(recvs_ls)
+
+    # maps the person_id to their index in the props or recvs arrays
+    mapback = [-1 for i in range(n)]
+    for i, id in enumerate(props):
+        mapback[id]=i
+    for i, id in enumerate(recvs):
+        mapback[id]=i
+
+    # tracks who the receivers are currently assigned to
+    current_matching = [-1 for i in recvs]
+
+    # helper function for GS alg - assigns a proposer to the receiver and pops elements 
+    # from receiver's ranking until current proposer is at the top
+    def pair(pr, rcv):
+        current_matching[mapback[rcv]] = pr
+        while recvs_ls[mapback[rcv]][-1] != pr:
+            recvs_ls[mapback[rcv]].pop()
+
+    # list tracking unmatched proposers
+    free_props = list(props)
+
+    # GS - translation of pseudo-code in the pdf
+    while(len(free_props)>0):
+        pr = free_props.pop()
+        rcv = prop_ls[mapback[pr]].pop()
+        if current_matching[mapback[rcv]] == -1:
+            pair(pr, rcv)
+        elif pr in recvs_ls[mapback[rcv]]:
+            free_props.append(current_matching[mapback[rcv]])
+            pair(pr, rcv)
+        else:
+            free_props.append(pr)
+
+    # collects the pairings
+    matches = [(current_matching[mapback[rcv]], rcv) for rcv in recvs]
+    print(matches)
     return matches
 
 if __name__ == "__main__":
